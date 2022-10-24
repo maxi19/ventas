445f26b63e372acdab39279689cf105c2c4b8dd1
maxi <maxi19@gmail.com>
2022-10-24 14:51:11 -0300

    fix data source

--- src/com/ventas/dao/Conexion.java
+++ src/com/ventas/dao/Conexion.java
@@ -4,14 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.omg.CORBA.TIMEOUT;
+
 public class Conexion {
 
-	private static final String URL = "jdbc:mysql://localhost:3306";
+	
+	private static final String HOST = "localhost";
+	private static final String URL = "jdbc:mysql://"+HOST+":3306";
 	private static final String DBNAME = "mercado";
+	private static final String TIMEZONE = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 				
 	private static final String USUARIO = "root";
 	private static final String PASSWORD = "root";
+	
 	
 	private static Conexion instance = Conexion.getInstance();
 	 
@@ -25,7 +31,7 @@ public class Conexion {
 		
 		try {
 			Class.forName(DRIVER);
-			conn = DriverManager.getConnection(URL+"/"+ DBNAME, USUARIO, PASSWORD);
+			conn = DriverManager.getConnection(URL+"/"+ DBNAME+TIMEZONE, USUARIO, PASSWORD);
 			if (!conn.isClosed()) {
 				System.out.println("conectado a la base de datos");
 			}	
